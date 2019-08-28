cd94e40d1946c7422dd9c12fb5bbc61bfae5fe81
GrantSteinfeld <grant.steinfeld@ibm.com>
2019-08-28 14:49:26 -0400

    min()

---
+++ streams/src/main/java/com/example/FindMin.java
@@ -0,0 +1,42 @@
+
+package com.example;
+
+//a simple program to demonstrate the use of stream in java 
+import java.util.*; 
+import java.util.stream.*; 
+  
+public class FindMin 
+{ 
+
+  private void findMinOldWay(int[] numbers){
+    
+    int min = numbers[0];
+    for (int i=1; i < numbers.length; i++){
+        if (min < numbers[i]){
+            min = numbers[i];
+        }
+    }
+
+    System.out.println("Min is " + min);
+  }
+
+  private void findMinStreams(int[] numbers) {
+    System.out.println("With Java 8 Streams");
+
+  }
+
+  private void run(){
+    int[] numbers = {4, 1, 13, 90, 16, 2, 0};
+
+    findMinOldWay(numbers);
+    findMinStreams(numbers);
+
+  }
+
+  public static void main(String args[]) 
+  { 
+    FindMin app = new FindMin();
+    app.run();
+   
+  } 
+} 
---
+++ streams/src/main/java/com/example/README.md
@@ -0,0 +1,6 @@
+
+### to compile
+``` shell
+cd streams/src/main/java
+javac com/example/Demonstration.java
+java com/example/Demonstration
