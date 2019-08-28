209ad54125bafc9c831a417cd897483fde58115f
GrantSteinfeld <grant.steinfeld@ibm.com>
2019-08-28 16:07:45 -0400

    Tutorial

--- streams/src/main/java/com/example/Tutorial.java
+++ streams/src/main/java/com/example/Tutorial.java
@@ -9,7 +9,7 @@ public class Tutorial
 { 
 
   private void findDistinctSmallestOldWay(int[] numbers){
-
+    
     //clone - avoid mutation orig array
     int[] copy = Arrays.copyOf( numbers, numbers.length);
 
@@ -21,16 +21,60 @@ public class Tutorial
         System.out.println(copy[i]);
     }
     
+    System.out.println("Distinct??? No");
+    //how to implement those - add more lines if you want
   }
 
+  private void findDistinctSmallestStreams(int[] numbers){
+      System.out.println("add code here")
+
+      //hints use:
+      //  distinct()
+      //  sorted()
+      //  limit()
+      //  forEach()
+      // pass printline function rather than call it
+
+  }
+
+  private void createIntStreams(int[] numbers){
+    System.out.println("add code here")
+
+    //hints use:
+    //  of()   // from param numbers array
+    //  range()
+    
+}
+
+private void createStreamsOther(){
+    Stream<String> streamEmpty = Stream.empty();
+
+    Collection<String> collection = Arrays.asList("a", "b", "c");
+    Stream<String> streamOfCollection = collection.stream();
+    
+}
   
+private void processIntStreams(int[] numbers){
+    System.out.println("add code here")
+
+    //hints use:
+    //  3 functions from findDistinctSmallestStreams() 
+    // skip() 
+    // filter()   // only evens; pass in lambda
+    // map()      // double each num; pass in lambda
+    
+}
 
 
   private void run(){
-    int[] numbers = {4, 100, 13, 90, 4, 16, 2, 88};
+    int[] numbers = {4, 100, 13, 90, 4, 16, 2, 89};
   
     findDistinctSmallestOldWay(numbers);
-    
+    findDistinctSmallestStreams(numbers);
+    createIntStreams();
+    createStreamsOther();
+    processIntStreams();
+
     
     
 
